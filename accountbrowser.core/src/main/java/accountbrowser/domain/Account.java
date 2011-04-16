a0b58f786b99ed3f2c8935dd6f472920fdf71a16
Tereza Moudra <t.moudra@gmail.com>
2011-04-16 12:59:34 +0200

    verze 2

---
+++ accountbrowser.core/src/main/java/accountbrowser/domain/Account.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package accountbrowser.domain;
+
+/**
+ * @author Student
+ * 
+ */
+public class Account {
+
+    private String state;
+    private int id;
+    private int balance;
+
+    private Owner owner;
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state
+     *            the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the balance
+     */
+    public int getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance
+     *            the balance to set
+     */
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+}
---
+++ accountbrowser.dao/src/main/java/accountbrowser/dao/DAOFactory.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package accountbrowser.dao;
+
+/**
+ * @author Student
+ * 
+ */
+public abstract class DAOFactory {
+
+    private static DAOFactory INSTANCE;
+
+    public static DAOFactory getInstance() {
+        if (INSTANCE == null) {
+            String property = System.getProperty("dao");
+            if ("hashmap".equals(property)) {
+                INSTANCE = new HashMapDAOFactory();
+            } else if ("mysql".equals(property)) {
+                // throw new
+            }
+        }
+        return INSTANCE;
+    }
+}
